ff76ac3b44135994a204ba3056025778e6a0d802
root <besckinder@gmail.com>
2023-08-09 15:50:50 -0700

    task 5 fix attempt

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,6 +4,7 @@ for i in range(0, 100):
         print("0{0}".format(i), end=", ")
     else:
         if i == 99:
-            print("{0}".format(i))
+            end="\n"
         else:
-            print(i, end=", ")
+            end=", "
+        print("{0}".format(i), end=end)

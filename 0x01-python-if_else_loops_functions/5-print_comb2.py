ee022bc50e953bf0a55607ca39725d7a43784913
root <besckinder@gmail.com>
2023-08-09 15:52:53 -0700

    Fixed pycodestyle issue

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,7 +4,7 @@ for i in range(0, 100):
         print("0{0}".format(i), end=", ")
     else:
         if i == 99:
-            end ="\n"
+            end = "\n"
         else:
-            end =", "
+            end = ", "
         print("{0}".format(i), end=end)

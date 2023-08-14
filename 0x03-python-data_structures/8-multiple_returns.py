09ed9f3cf38138d667cf8a49c0ebd26a3c2d0f9c
root <besckinder@gmail.com>
2023-08-14 06:03:53 -0700

    checker fix attempt 1

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    t = (len(sentence), sentence[0])
-    if sentence is None:
-        t = (len(sentence), None)
+    if sentence == "":
+        t = (0, None)
+    else:
+        t = (len(sentence), sentence[0])
     return t[0], t[1]
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if my_list == []:
         return None
     else:
         max_int = my_list[0]

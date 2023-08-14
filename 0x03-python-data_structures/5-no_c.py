6e8ccf42bc1740d6c248fe9d08295fdd2206226e
root <besckinder@gmail.com>
2023-08-14 07:18:32 -0700

    Checker fix attempt 1

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_str = ""
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            return my_string[:i] + my_string[i+1:]
+            new_str +=  my_string[:i] + my_string[i+1:]
         else:
             continue
+    return new_str

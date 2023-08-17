31a7c3e126d214ade5931f77bf0aa5fe56a3dc81
root <besckinder@gmail.com>
2023-08-16 20:00:13 -0700

    Debugging

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     if value not in a_dictionary.values():
         return a_dictionary
-    keys_to_delete= []
+    keys_to_delete = []
     for i, j in a_dictionary.items():
         if j == value:
             keys_to_delete.append(i)

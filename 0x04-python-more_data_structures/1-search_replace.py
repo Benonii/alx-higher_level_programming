9dfaacdcdf85610a394f3f71f84980698b5f628d
root <besckinder@gmail.com>
2023-08-16 19:34:57 -0700

    pycodestyle fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for i in range (len(my_list)):
+    for i in range(len(my_list)):
         if my_list[i] == search:
             new_list.append(replace)
         else:

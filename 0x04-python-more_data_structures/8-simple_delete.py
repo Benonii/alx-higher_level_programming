603f59d2e9da2f04f769836f223981c16cf28625
root <besckinder@gmail.com>
2023-08-16 19:39:28 -0700

    Checker fix attempt 3

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,5 +2,6 @@
 def simple_delete(a_dictionary, key=""):
     if key not in a_dictionary.keys():
         return a_dictionary
-    del a_dictionary[key]
-    return a_dictionary
+    else:
+        del a_dictionary[key]
+        return a_dictionary

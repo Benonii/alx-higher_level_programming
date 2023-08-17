0b934725f151f60e6b98b0e05e291b8ff508145b
root <besckinder@gmail.com>
2023-08-16 19:44:00 -0700

    pycodestyle fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return None   
+        return None
     keys = list(a_dictionary.keys())
 
     max = a_dictionary[keys[0]]

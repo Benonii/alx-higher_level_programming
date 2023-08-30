dcea9ceec7569c39d041eebe9794dfb270ec302f
root <besckinder@gmail.com>
2023-08-30 02:48:42 +0000

    More Doc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,5 +4,7 @@ class Square:
     ''' a class that initalizes an empty variable size '''
 
     def __init__(self, size=0):
-       '''assignes size '''
+       '''This function initializes the private field size
+          and assignes it to the inputted numebr
+        '''
        self.__size = size

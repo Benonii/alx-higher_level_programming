dee6b012c1ef6dd39e16bcc548de14a6c5f089af
root <besckinder@gmail.com>
2023-08-30 02:02:57 +0000

    Mandatory tasks completed

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+    try:
+        def __init__(self, size=0):
+            if isinstance(size, int):
+                if size < 0:
+                    raise ValueError("size must be >= 0")
+                else:
+                    self.__size = size
+            else:
+                raise TypeError("size must be an integer")
+    except TypeError as te:
+        print(te)
+    except ValueError as ve:
+        print(ve)
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+    def area(self):
+        return self.__size ** 2

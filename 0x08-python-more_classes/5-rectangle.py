5d92f813fb5878267e09b07fe0bf2c8473023ce9
root <besckinder@gmail.com>
2023-09-04 18:30:25 -0700

    final fix

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -52,22 +52,23 @@ class Rectangle:
         ''' calculates the perimeter of the rectangle '''
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self__width + self.__height)
+        return 2 * (self.__width + self.__height)
 
     def __str__(self):
         ''' returns a rectangle made up of ``#`` '''
         rectangle = ""
         if self.__width == 0 or self.__height == 0:
             return rectangle
-        for i in range(self.__hegiht):
-            for j in range(self.__width):
-                rectangle += rectangle + "#" 
-            rectangle += rectangle + "\n"
+        for height in range(self.__height):
+            for width in range(self.__width):
+                rectangle += rectangle + "#"
+            if height != str.__height - 1:
+                rectangle += rectangle + "\n"
         return rectangle
 
     def __repr__(self):
         ''' returns a replicable string representation of a rectangle '''
-        rectangle = 'Rectangle(' + str(self.__width) + ',' str(self.__height)\
+        rectangle = 'Rectangle(' + str(self.__width) + ',' + str(self.__height)\
                     + ')' 
         return rectangle
 

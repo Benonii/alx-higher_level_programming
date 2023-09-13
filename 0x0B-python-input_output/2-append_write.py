ad4e07d1c2a8bed27ea94df3b4041a279fd554a1
root <besckinder@gmail.com>
2023-09-12 19:10:06 -0700

    checker fix attempt 3

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,7 +8,9 @@ def append_write(filename="", text=""):
     ''' appends ``text`` to the file ``filename``
         doesn't handle any exceptions '''
 
+    chars = 0
     with open(filename, 'a', encoding='utf-8') as f:
         for char in text:
             f.write(char)
-        return f.tell()
+            chars += 1
+    return chars

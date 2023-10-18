f54c14417998ae4c6a9c64c64d91bebafd55d7fb
root <besckinder@gmail.com>
2023-10-17 19:49:49 -0700

    Adding more commands to hange table and name

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,5 @@
 -- Converts hbtn_0_C databse to UTF-8
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

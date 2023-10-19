6d18c3eb0af47a6cfe67a1b0106b602779ae9ec6
root <besckinder@gmail.com>
2023-10-19 00:01:27 -0700

    checker fix attempt 1

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,6 +2,6 @@
 SELECT DISTINCT tv_genres.name AS name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
-LEFT JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id
-WHERE tv_shows.title IS NULL OR tv_shows.title = "Dexter"
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title IS NULL OR tv_shows.title != "Dexter"
 ORDER BY name ASC;
---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,7 @@
+-- Uses hbtn_0d_tvshows database to list all shows of the genre Comedy
+SELECT DISTINCT tv_shows.title AS title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name IS NULL OR tv_genres.name != "Comedy"
+ORDER BY title ASC;

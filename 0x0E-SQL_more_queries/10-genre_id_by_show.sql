cfed639a7fb9f1b82a52fed1834a9a8469406c45
root <besckinder@gmail.com>
2023-10-18 19:18:27 -0700

    Checking early

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

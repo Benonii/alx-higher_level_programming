3b2bf34a745e1e614f7fa881be84d757555d3191
root <besckinder@gmail.com>
2023-10-18 19:22:59 -0700

    Fixed ON statement

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,6 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows INNER JOIN tv_show_genres
-ON tv_show_genres.genre_id IS NOT NULL
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;

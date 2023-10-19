d42687d9999d64aae65d5e12d0acc4f5bfa47652
root <besckinder@gmail.com>
2023-10-18 23:55:26 -0700

    as -> AS

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all shows contained in hbtn_0d_tvshows with genre ID if it exists
 
-SELECT tv_shows.title AS title, tv_genres.name as name
+SELECT tv_shows.title AS title, tv_genres.name AS name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id

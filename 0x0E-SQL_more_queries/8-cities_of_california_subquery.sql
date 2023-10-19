aaee6f27cfaf54b1c0c1b8a81e6d44551b8d7a54
root <besckinder@gmail.com>
2023-10-18 18:40:05 -0700

    Subquery from states not cites

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Lists all the cities of California that can be found in the databse hbtn_0d_usa
 SELECT * FROM cities 
-WHERE cities.state_id = (SELECT id FROM cities WHERE name = "California")
+WHERE cities.state_id = (SELECT id FROM states WHERE name = "California")
 ORDER BY cities.id ASC;

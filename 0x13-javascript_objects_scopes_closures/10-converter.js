666d7744f86844b98efd91fa40514c244ca9f769
root <besckinder@gmail.com>
2023-11-14 13:34:50 -0800

    scopes, scopes scopes

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,15 @@
+exports.converter = function (base) {
+  const symbols = '0123456789abcdef';
+
+  function convertToBase (number) {
+    if (number < base) {
+      return symbols[number];
+    } else {
+      const quotient = Math.floor(number / base);
+      const remainder = number % base;
+      return convertToBase(quotient) + symbols[remainder];
+    }
+  }
+
+  return (convertToBase);
+};

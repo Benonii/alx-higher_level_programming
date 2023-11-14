a9f724ce74915d49f9bfffba0b8f99adc6bdd735
root <besckinder@gmail.com>
2023-11-14 02:44:28 +0000

    semistandard fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ const firstArg = argv[2];
 const x = Math.floor(Number(firstArg));
 
 if (Number.isNaN(x)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number: ' + x);
+  console.log('My number: ' + x);
 }

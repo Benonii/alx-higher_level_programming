5b93e38eae0217a7a4850d6386352b7d38d35288
root <besckinder@gmail.com>
2023-11-17 10:03:19 -0800

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,33 @@
--- Create states table in hbtn_0e_0_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-USE hbtn_0e_0_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+#!/usr/bin/python3
+
+''' This script selects all states from the database hbtn_0e_0_usa '''
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    av = sys.argv
+    username = av[1]
+    password = av[2]
+    dbname = av[3]
+    searched = av[4]
+
+    try:
+        db = MySQLdb.connect(user=username, password=password, database=dbname,
+                             host="localhost", port=3306)
+
+        c = db.cursor()
+        c.execute("""SELECT DISTINCT MIN(id), name FROM states WHERE name = %s\
+                     ORDER BY id;""", (searched,))
+
+        rows = c.fetchall()
+
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+    finally:
+        if db:
+            db.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,33 @@
--- Create states table in hbtn_0e_4_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
-USE hbtn_0e_4_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+#!/usr/bin/python3
 
-CREATE TABLE IF NOT EXISTS cities ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(state_id) REFERENCES states(id)
-);
-INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
-INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
-INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
-INSERT INTO cities (state_id, name) VALUES (4, "New York");
-INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
+''' This script selects all states from the database hbtn_0e_0_usa '''
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    av = sys.argv
+    username = av[1]
+    password = av[2]
+    dbname = av[3]
+
+    try:
+        db = MySQLdb.connect(user=username, password=password, database=dbname,
+                             host="localhost", port=3306)
+
+        c = db.cursor()
+        c.execute("SELECT DISTINCT cities.id, cities.name, states.name\
+                   FROM cities INNER JOIN states\
+                   ON cities.state_id = states.id ORDER BY cities.id;")
+
+        rows = c.fetchall()
+
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+    finally:
+        if db:
+            db.close()

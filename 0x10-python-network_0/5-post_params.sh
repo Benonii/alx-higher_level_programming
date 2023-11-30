bec0542f6a79ce2c6e6473e89314dbd49f41a3b8
root <besckidner@gmail.com>
2023-11-30 15:32:24 -0800

    : => =

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a post request to the provided ip
-curl -s -X POST -d "email:test@gmail.com&subject:I will always be here for pld" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for pld" "$1"

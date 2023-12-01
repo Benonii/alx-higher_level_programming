9c0a65782296d327022a56dc56479318b8435485
Benonii <besckinder@gmail.com>
2023-12-01 15:25:03 -0800

    Last try

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,13 @@
 
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
 
-    print(html)
+        print(html)
 
-    print("Body response:")
-    print(f"    - type: {type(html)}")
-    print(f"    - content: {html}")
-    print(f"    - utf8 content: {html.decode('utf-8')}")
+        print("Body response:")
+        print(f"    - type: {type(html)}")
+        print(f"    - content: {html}")
+        print(f"    - utf8 content: {html.decode('utf-8')}")

a479c64d29a3ba85372aef75a516e01cada45d66
Benonii <besckinder@gmail.com>
2023-12-01 15:17:47 -0800

    Don't execute if not main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,7 @@
 import sys
 import urllib.request
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    request_id = response.headers.get('X-Request-Id')
-    print(request_id)
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        request_id = response.headers.get('X-Request-Id')
+        print(request_id)

efd29995666edece8564ae7c8dd506597106c999
Benonii <besckinder@gmail.com>
2023-12-02 11:48:20 -0800

    Done

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' Handles error codes '''
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    if requests.get(sys.argv[1]).status_code >= 400:
+        print("Error code: ", requests.get(sys.argv[1]).status_code)
+    else:
+        print(response.text)

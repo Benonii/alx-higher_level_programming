f7a5ef3145af66796b14fed802e67b3387631755
Benonii <besckinder@gmail.com>
2023-12-02 11:55:53 -0800

    Removed excess space

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,6 +6,6 @@ import requests
 
 if __name__ == '__main__':
     if requests.get(sys.argv[1]).status_code >= 400:
-        print("Error code: ", requests.get(sys.argv[1]).status_code)
+        print("Error code:", requests.get(sys.argv[1]).status_code)
     else:
         print(requests.get(sys.argv[1]).text)

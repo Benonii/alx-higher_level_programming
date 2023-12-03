6cd7f1953a58ded0c343ac925ef821571f762cde
Benonii <besckinder@gmail.com>
2023-12-03 12:19:39 -0800

    This gotta work

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,7 @@ if __name__ == '__main__':
             response:
         html = response.read()
 
-        print(html)
-
         print("Body response:")
-        print(f"- type: {type(html)}")
-        print(f"- content: {html}")
-        print(f"- utf8 content: {html.decode('utf-8')}\n")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")

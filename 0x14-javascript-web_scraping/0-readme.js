e28c828c1d59f3fd01f39cab4f393062f81e7acf
root <besckinder@gmail.com>
2024-01-03 05:39:16 -0800

    First commit

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', (err, data) => {
+  if (err) {
+    process.exit(1);
+  }
+
+  console.log(data);
+});
